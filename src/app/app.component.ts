5944372fa6d9c5089e79525f42fee5bf96c46426
Jorge Jara <jorge18xdd@gmail.com>
2024-03-19 16:06:08 -0500

    declarationApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'DeclarationApp';
+
+
+  @ViewChild('asCard')
+  public card!:ElementRef<HTMLDivElement>
+
+  public heart:number = 0
+
+  constructor(private rendered2:Renderer2){
+
+  }
+
+
+  NoDecision(){
+
+     const value1 = Math.random() * (75 - 0) - 0;
+     const value2 = Math.random() * (75 - 0) - 0;
+     const noResp = this.card.nativeElement
+
+
+     this.rendered2.removeClass(noResp,'card')
+     this.rendered2.addClass(noResp,'card')
+     this.rendered2.setStyle(noResp,'left',`${value1.toString()}%`)
+     this.rendered2.setStyle(noResp,'top',`${value2.toString()}%`)
+
+     this.heart += 1
+    }
 }
